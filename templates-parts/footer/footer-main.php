2621977d33d15f7ac5fb4e6a3a5da6a8f1b13d8e
Creastv <piotr@roial.pl>
2023-02-10 09:51:07 +0100

    fixing

--- templates-parts/footer/footer-main.php
+++ templates-parts/footer/footer-main.php
@@ -2,7 +2,7 @@
     <div class="f-main__wraper">
         <div class="f-main__col">
             <p>Komornik Sądowy przy Sądzie Rejonowym w Wyszkowie Krzysztof Maciej Mazuchowski</p>
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+            <?php wp_nav_menu( array( 'theme_location' => 'secundary_menu' ) ); ?>
         </div>
         <div class="f-main__col">
             <p>Kancelaria wykorzystuje następujące systemy</p>
